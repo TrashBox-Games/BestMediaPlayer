bfbc00ff31af738b1195c978050fe1f1e7364147
Ezekiel Mohr <85984467+Ezmr7@users.noreply.github.com>
2025-03-06 17:50:20 -0600

    Added react-native-track-player

--- index.ts
+++ index.ts
@@ -1,6 +1,10 @@
 import { registerRootComponent } from "expo";
+import TrackPlayer from "react-native-track-player";
 
 import App from "./App";
+
+// Register the TrackPlayer service
+TrackPlayer.registerPlaybackService(() => require('./service.js'));
 
 // Register the main component
 registerRootComponent(App);
