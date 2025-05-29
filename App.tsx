f57ef2c680d283a12e93008dae92917edca2a292
Ezekiel Mohr <85984467+Ezmr7@users.noreply.github.com>
2025-05-29 13:14:22 -0500

    Added database provider to App and added new screens

--- App.tsx
+++ App.tsx
@@ -6,14 +6,18 @@ import { StatusBar } from "expo-status-bar";
 import { Ionicons } from "@expo/vector-icons";
 import TrackPlayer from "react-native-track-player";
 // Import screens
-import PlaylistsScreen from "./screens/PlaylistsScreen";
+import PlaylistsScreen from "./screens/PlaylistListScreen";
 import VideoScreen from "./screens/VideoScreen";
 import SettingsScreen from "./screens/SettingsScreen";
 import AudioScreen from "./screens/AudioScreen";
 import AddFileScreen from "./screens/AddFileScreen";
+import PlaylistScreen from "./screens/PlaylistScreen";
+import CreatePlaylistScreen from "./screens/CreatePlaylistScreen";
 // Import context provider
 import { TagsProvider } from "./contexts/TagsContext";
 import { PlayerProvider } from "./contexts/PlayerContext";
+import { SQLiteProvider } from "expo-sqlite";
+import { init } from "./db/queries";
 
 // Define the tab navigator parameter list type
 type TabParamList = {
@@ -22,7 +26,8 @@ type TabParamList = {
   Audio: undefined;
   "Add Files": undefined;
   Video: undefined;
-  "Http Server": undefined;
+  Playlist: undefined;
+  "Create Playlist": undefined;
 };
 
 const Tab = createBottomTabNavigator<TabParamList>();
@@ -34,63 +39,88 @@ export default function App() {
   );
 
   return (
-    <TagsProvider>
-      <PlayerProvider>
-        <View style={styles.container}>
-          <StatusBar style={themeMode === "dark" ? "light" : "dark"} />
-          <NavigationContainer>
-            <Tab.Navigator
-              screenOptions={({ route }) => ({
-                tabBarIcon: ({ focused, color, size }) => {
-                  let iconName: any;
+    <SQLiteProvider databaseName="bestmedia.db" onInit={init}>
+      <TagsProvider>
+        <PlayerProvider>
+          <View style={styles.container}>
+            <StatusBar style={themeMode === "dark" ? "light" : "dark"} />
+            <NavigationContainer>
+              <Tab.Navigator
+                screenOptions={({ route }) => ({
+                  tabBarIcon: ({ focused, color, size }) => {
+                    let iconName: any;
 
-                  if (route.name === "Playlists") {
-                    iconName = focused ? "list" : "list-outline";
-                  } else if (route.name === "Audio") {
-                    iconName = focused
-                      ? "musical-notes"
-                      : "musical-notes-outline";
-                  } else if (route.name === "Video") {
-                    iconName = focused ? "videocam" : "videocam-outline";
-                  } else if (route.name === "Settings") {
-                    iconName = focused ? "settings" : "settings-outline";
-                  } else if (route.name === "Add Files") {
-                    iconName = focused ? "add-circle" : "add-circle-outline";
-                  }
-                  // Return the Ionicons component
-                  return <Ionicons name={iconName} size={size} color={color} />;
-                },
-                tabBarActiveTintColor:
-                  themeMode === "dark" ? "#FFFFFF" : "#007AFF",
-                tabBarInactiveTintColor:
-                  themeMode === "dark" ? "#8E8E93" : "#8E8E93",
-                headerShown: true,
-                tabBarStyle: {
-                  backgroundColor: themeMode === "dark" ? "#1C1C1E" : "#FFFFFF",
-                  borderTopColor: themeMode === "dark" ? "#38383A" : "#E5E5EA",
-                },
-                headerStyle: {
-                  backgroundColor: themeMode === "dark" ? "#1C1C1E" : "#FFFFFF",
-                },
-                headerTintColor: themeMode === "dark" ? "#FFFFFF" : "#000000",
-              })}
-            >
-              <Tab.Screen name="Playlists" component={PlaylistsScreen} />
-              <Tab.Screen name="Audio" component={AudioScreen} />
-              <Tab.Screen name="Add Files" component={AddFileScreen} />
-              <Tab.Screen name="Video" component={VideoScreen} />
-              <Tab.Screen
-                name="Settings"
-                component={SettingsScreen}
-                options={{
-                  tabBarLabel: "Settings",
-                }}
-              />
-            </Tab.Navigator>
-          </NavigationContainer>
-        </View>
-      </PlayerProvider>
-    </TagsProvider>
+                    if (route.name === "Playlists") {
+                      iconName = focused ? "list" : "list-outline";
+                    } else if (route.name === "Audio") {
+                      iconName = focused
+                        ? "musical-notes"
+                        : "musical-notes-outline";
+                    } else if (route.name === "Video") {
+                      iconName = focused ? "videocam" : "videocam-outline";
+                    } else if (route.name === "Settings") {
+                      iconName = focused ? "settings" : "settings-outline";
+                    } else if (route.name === "Add Files") {
+                      iconName = focused ? "add-circle" : "add-circle-outline";
+                    } else if (route.name === "Playlist") {
+                      iconName = focused ? "disc" : "disc-outline";
+                    } else if (route.name === "Create Playlist") {
+                      iconName = focused ? "add-circle" : "add-circle-outline";
+                    }
+                    // Return the Ionicons component
+                    return (
+                      <Ionicons name={iconName} size={size} color={color} />
+                    );
+                  },
+                  tabBarActiveTintColor:
+                    themeMode === "dark" ? "#FFFFFF" : "#007AFF",
+                  tabBarInactiveTintColor:
+                    themeMode === "dark" ? "#8E8E93" : "#8E8E93",
+                  headerShown: true,
+                  tabBarStyle: {
+                    backgroundColor:
+                      themeMode === "dark" ? "#1C1C1E" : "#FFFFFF",
+                    borderTopColor:
+                      themeMode === "dark" ? "#38383A" : "#E5E5EA",
+                  },
+                  headerStyle: {
+                    backgroundColor:
+                      themeMode === "dark" ? "#1C1C1E" : "#FFFFFF",
+                  },
+                  headerTintColor: themeMode === "dark" ? "#FFFFFF" : "#000000",
+                })}
+              >
+                <Tab.Screen name="Playlists" component={PlaylistsScreen} />
+                <Tab.Screen name="Audio" component={AudioScreen} />
+                <Tab.Screen
+                  name="Playlist"
+                  component={PlaylistScreen}
+                  options={{
+                    title: "Featured Playlist",
+                  }}
+                />
+                <Tab.Screen name="Add Files" component={AddFileScreen} />
+                <Tab.Screen name="Video" component={VideoScreen} />
+                <Tab.Screen
+                  name="Settings"
+                  component={SettingsScreen}
+                  options={{
+                    tabBarLabel: "Settings",
+                  }}
+                />
+                <Tab.Screen
+                  name="Create Playlist"
+                  component={CreatePlaylistScreen}
+                  options={{
+                    title: "Create Playlist",
+                  }}
+                />
+              </Tab.Navigator>
+            </NavigationContainer>
+          </View>
+        </PlayerProvider>
+      </TagsProvider>
+    </SQLiteProvider>
   );
 }
 
